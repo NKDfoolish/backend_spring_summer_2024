67d793296e72f8836aee628162340dc5567f62a4
NKDfoolish <nguyenkhanhduy.dilinh@gmail.com>
2024-08-18 23:31:17 +0700

    update 18.8.24.v1

--- src/main/java/com/nkd/identity_service/controller/UserController.java
+++ src/main/java/com/nkd/identity_service/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.nkd.identity_service.controller;
 
+import com.nkd.identity_service.dto.request.ApiResponse;
 import com.nkd.identity_service.dto.request.UserCreationRequest;
 import com.nkd.identity_service.dto.request.UserUpdateRequest;
 import com.nkd.identity_service.entity.User;
 import com.nkd.identity_service.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +18,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody UserCreationRequest request){
-        return userService.createUser(request);
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request){
+        ApiResponse<User> apiResponse = new ApiResponse<>();
+
+        apiResponse.setResult(userService.createUser(request));
+
+        return apiResponse;
     }
 
     @GetMapping
