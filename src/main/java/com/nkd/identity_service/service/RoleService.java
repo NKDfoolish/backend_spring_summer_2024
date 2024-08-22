0077494ced6725235341f07909b2f7d12ab00ff9
NKDfoolish <nguyenkhanhduy.dilinh@gmail.com>
2024-08-22 22:40:11 +0700

    22.8.24.v2

---
+++ src/main/java/com/nkd/identity_service/service/RoleService.java
@@ -0,0 +1,45 @@
+package com.nkd.identity_service.service;
+
+import com.nkd.identity_service.dto.request.RoleRequest;
+import com.nkd.identity_service.dto.response.RoleResponse;
+import com.nkd.identity_service.mapper.RoleMapper;
+import com.nkd.identity_service.repository.PermissionRepository;
+import com.nkd.identity_service.repository.RoleRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RoleService {
+    RoleRepository roleRepository;
+    RoleMapper roleMapper;
+    PermissionRepository permissionRepository;
+
+    public RoleResponse create(RoleRequest request){
+        var role = roleMapper.toRole(request);
+
+        var permissions = permissionRepository.findAllById(request.getPermissions());
+        role.setPermissions(new HashSet<>(permissions));
+
+        role = roleRepository.save(role);
+
+        return roleMapper.toRoleResponse(role);
+    }
+
+    public List<RoleResponse> getAll(){
+        var roles = roleRepository.findAll();
+        return roles.stream().map(roleMapper::toRoleResponse).toList();
+    }
+
+    public void delete(String role){
+        roleRepository.deleteById(role);
+    }
+}
